e767a507482d42461e728a7198d7d36ea1c42ae3
yeonji3038 <rokmc3038@naver.com>
2024-12-03 14:37:40 +0900

    프로젝트 생성

---
+++ src/test/java/ssafy/StackFlow/StackFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.StackFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
