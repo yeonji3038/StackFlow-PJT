337c4c0b9c0164f334bca7ceaf244c0e5774041a
jeongin <harperim127@gmail.com>
2024-12-22 07:03:02 +0900

    241222 U complete

--- BACK/src/main/java/ssafy/StackFlow/Controller/notice/NoticeController.java
+++ BACK/src/main/java/ssafy/StackFlow/Controller/notice/NoticeController.java
@@ -2,16 +2,23 @@ package ssafy.StackFlow.Controller.notice;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ssafy.StackFlow.Domain.Notice;
+import ssafy.StackFlow.Domain.user.Signup;
 import ssafy.StackFlow.Service.NoticeService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ssafy.StackFlow.Service.user.UserService;
 
 import javax.naming.Binding;
+import java.security.Principal;
 import java.util.List;
 
 @RequestMapping("/notice")
@@ -20,9 +27,13 @@ import java.util.List;
 public class NoticeController {
 
     private final NoticeService noticeService;
+    private final UserService userService;
 
     @GetMapping("")
-    public String list(Model model) {
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Notice> paging = this.noticeService.getList(page);
+        model.addAttribute("paging", paging);
+
         List<Notice> noticeList = this.noticeService.getList();
         model.addAttribute("noticeList", noticeList);
         return "notice";
@@ -31,24 +42,60 @@ public class NoticeController {
     @GetMapping(value = "/{id}")
     public String detail(Model model, @PathVariable("id") Long id) {
         Notice notice = this.noticeService.getNotice(id);
+
         model.addAttribute("notice", notice);
         return "notice_detail";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     public String noticeCreate(NoticeForm noticeForm) {
         return "notice_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
     public String noticeCreate(@Valid NoticeForm noticeForm,
-                               BindingResult bindingResult) {
+                               BindingResult bindingResult,
+                               Principal principal) {
         if (bindingResult.hasErrors()) {
             return "notice_form";
         }
+        Signup signup = this.userService.getUser(principal.getName());
         this.noticeService.create(noticeForm.getTitle(),
-                noticeForm.getContent());
+                noticeForm.getContent(), signup);
         return "redirect:/notice";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String noticeModify(NoticeForm noticeForm,
+                               @PathVariable("id") Long id,
+                               Principal principal) {
+        Notice notice = this.noticeService.getNotice(id);
+        if(!notice.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        noticeForm.setTitle(notice.getTitle());
+        noticeForm.setContent(notice.getContent());
+        return "notice_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String noticeModify(@Valid NoticeForm noticeForm,
+                                 BindingResult bindingResult,
+                                 Principal principal,
+                                 @PathVariable("id") Long id) {
+        if (bindingResult.hasErrors()) {
+            return "notice_form";
+        }
+        Notice notice = this.noticeService.getNotice(id);
+        if (!notice.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.noticeService.modify(notice, noticeForm.getTitle(), noticeForm.getContent());
+        return String.format("redirect:/notice/detail/%s", id);
     }
 
 }
--- BACK/src/main/java/ssafy/StackFlow/Service/NoticeService.java
+++ BACK/src/main/java/ssafy/StackFlow/Service/NoticeService.java
@@ -2,12 +2,18 @@ package ssafy.StackFlow.Service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ssafy.StackFlow.Domain.File;
 import ssafy.StackFlow.Domain.Notice;
+import ssafy.StackFlow.Domain.user.Signup;
 import ssafy.StackFlow.Repository.NoticeRepository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,12 +36,27 @@ public class NoticeService {
         }
     }
 
-    public void create(String title, String content) {
+    public void create(String title, String content, Signup user) {
         Notice n = new Notice();
         n.settitle(title);
         n.setcontent(content);
         n.setcreatedAt(LocalDateTime.now());
         n.setupdatedAt(LocalDateTime.now());
+        n.setAuthor(user);
         this.noticeRepository.save(n);
+    }
+
+    public Page<Notice> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createdAt"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.noticeRepository.findAll(pageable);
+    }
+
+    public void modify(Notice notice, String title, String content) {
+        notice.settitle(title);
+        notice.setcontent(content);
+        notice.setupdatedAt(LocalDateTime.now());
+        this.noticeRepository.save(notice);
     }
 }
