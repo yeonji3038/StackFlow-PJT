7a982a4d23c3d1be64899402d3ef5a8ce1cac311
kjm1203 <kwonjm1203@naver.com>
2024-12-23 21:07:09 +0900

    채개인채팅 생성 고침

--- BACK/src/main/java/ssafy/StackFlow/Controller/chat/ChatController.java
+++ BACK/src/main/java/ssafy/StackFlow/Controller/chat/ChatController.java
@@ -5,8 +5,12 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import ssafy.StackFlow.Domain.chat.ChatMessage;
 import ssafy.StackFlow.Service.chat.ChatService;
+
+import java.security.Principal;
 
 
 @Controller
@@ -20,5 +24,13 @@ public class ChatController {
     public ChatMessage send(ChatMessage message) {
         chatService.saveMessage(message);
         return message;
+    }
+
+    @GetMapping("/chat")
+    public String getChatPage(Model model, Principal principal) {
+        if (principal != null) {
+            model.addAttribute("currentUserId", principal.getName());
+        }
+        return "chat";
     }
 }
