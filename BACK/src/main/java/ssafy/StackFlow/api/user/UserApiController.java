5f20036bbf116a995368e64c99aac43d58711567
최연지 <rokmc3038@naver.com>
2024-12-24 21:37:07 +0900

    회원가입API 연결

---
+++ BACK/src/main/java/ssafy/StackFlow/api/user/UserApiController.java
@@ -0,0 +1,24 @@
+package ssafy.StackFlow.api.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ssafy.StackFlow.Domain.user.Signup;
+import ssafy.StackFlow.Service.DataNotFoundException;
+import ssafy.StackFlow.Service.user.UserService;
+
+@RestController
+@RequiredArgsConstructor
+public class UserApiController {
+
+    private final UserService userService; // UserService 주입
+
+    @PostMapping("/api/user/signup")
+    public ResponseEntity<String> signup(@RequestBody UserDto userDto) {
+        userService.signup(userDto);
+        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+    }
+}
---
+++ BACK/src/main/java/ssafy/StackFlow/api/user/UserDto.java
@@ -0,0 +1,31 @@
+package ssafy.StackFlow.api.user;
+
+
+import lombok.Data;
+
+
+@Data
+public class UserDto {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    private String storeCode;
+
+}
+
+
+//DTO(Data Transfer Object)
+// 실제 엔터티를 객체를 통해 데이터를 전달하지 않고 DTO를 톻해 데이터를 주고 받음
+
+//@NoArgsConstructor: 파라미터가 없는 기본 생성자를 자동으로 생성
+//JPA 엔티티 클래스에서 사용 -> JPA 스펙에 따라 기본 생성자가 필요
+//Bean 생성 ->  빈 등록 시 기본 생성자가 필요
+//DTO 클래스 -> 기본 생성자를 이용해 객체를 생성한 후, setter를 통해 값을 설정해야 할 때 사용
+
+
+//@AllArgsConstructor: 클래스의 모든 필드를 매개변수로 받는 생성자를 자동으로 생성
+// 객체 생성시 모든 필드 한번에 초기화 할때 사용
+// 값을 변경 할수 없는 클래스 설계 할때 생성자 통해 필드 초기회 할때 사용
+// 테스트 및 간단한 초기화
