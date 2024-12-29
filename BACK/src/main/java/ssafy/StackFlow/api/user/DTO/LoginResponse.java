6542bd60e8f779885fabfa9a0cfdbbd12859fb17
kjm1203 <kwonjm1203@naver.com>
2024-12-30 00:16:02 +0900

    로ê·로그ìapi 추가

--- BACK/src/main/java/ssafy/StackFlow/api/user/DTO/LoginResponse.java
+++ BACK/src/main/java/ssafy/StackFlow/api/user/DTO/LoginResponse.java
@@ -2,12 +2,13 @@ package ssafy.StackFlow.api.user.DTO;
 
 import lombok.Getter;
 import lombok.Setter;
+import ssafy.StackFlow.Domain.Store;
 import ssafy.StackFlow.Domain.user.Signup;
 
 @Getter @Setter
 public class LoginResponse {
     private String message;
     private Signup user;
+    private Store store;
     private String jsessionId;
-
 }
