9bc631d50eef5e96e40ca2b7d58c244bf774cd96
서현석 <jacobshs4824@naver.com>
2025-01-02 12:01:22 +0900

    auth 기능 개발

--- FRONT/stackflow/src/index.js
+++ FRONT/stackflow/src/index.js
@@ -4,28 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals'
 import { CookiesProvider} from "react-cookie"
+import store from './store/index'
 import { Provider } from "react-redux"
-import { createStore } from "redux"
+import { ConfigProvider } from "./store/index";
 
-const csrfToken = "94C051EA049127D48B14FFB37DEAAC43"
-const BASE_URL = "http://localhost:8080"
-const useState = {
-  csrfToken : csrfToken,
-  BASE_URL : BASE_URL
-}
-
-function reducer(state = useState, action){
-  return state
-}
-
-let store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <CookiesProvider>
-        <App /> 
+        <ConfigProvider>
+          <App /> 
+        </ConfigProvider>
       </CookiesProvider>
     </Provider>
   </React.StrictMode>
---
+++ FRONT/stackflow/src/store/PrivateRoute.js
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import useAuth from './tokenManage'
+
+const PrivateRoute = () => {
+  const { getToken } = useAuth();
+  const token = getToken();
+
+  return token ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
