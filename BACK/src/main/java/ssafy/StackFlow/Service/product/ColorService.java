4a97725183b75cc7aeb37316a57d1dad48960edc
최연지 <rokmc3038@naver.com>
2025-01-03 00:12:50 +0900

    컬러코드

--- BACK/src/main/java/ssafy/StackFlow/Service/product/ColorService.java
+++ BACK/src/main/java/ssafy/StackFlow/Service/product/ColorService.java
@@ -3,6 +3,7 @@ package ssafy.StackFlow.Service.product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ssafy.StackFlow.Domain.category.Category;
 import ssafy.StackFlow.Domain.product.Color;
 import ssafy.StackFlow.Domain.product.Product;
 import ssafy.StackFlow.Repository.product.ColorRepository;
@@ -23,5 +24,9 @@ public class ColorService {
         Color color = colorRepository.findByColorCode(colorCode)
             .orElseThrow(() -> new IllegalArgumentException("해당 색상 코드가 없습니다."));
         return color.getProducts();
+    }
+    @Transactional // 이 메서드는 데이터베이스에 변경을 가하므로 트랜잭션을 활성화합니다.
+    public void save(Color color) {
+        colorRepository.save(color); // 카테고리 저장
     }
 }
