bb315a701d2ad855cc3e2873014459e68a3c0388
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-02-28 20:26:26 +0900

    fix : Swagger 설정

--- BACK/src/main/java/ssafy/StackFlow/global/config/SwaggerConfig.java
+++ BACK/src/main/java/ssafy/StackFlow/global/config/SwaggerConfig.java
@@ -7,55 +7,34 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    private Info apiInfo() {
-        return new Info()
-                .title("StackFlow API")
-                .description("StackFlow API 명세서")
-                .version("v1.0")
-                .contact(new Contact().name("StackFlow")
-                        .email("www.StackFlow.com")
-                        .url("rokmc17047200@gmail.com"))
-                .license(new License()
-                        .name("License of API")
-                        .url("API license URL"));
-    }
-
-    private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                .bearerFormat("JWT")
-                .scheme("bearer");
-    }
-
     @Bean
     public OpenAPI openAPI() {
+        // Define security scheme
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Create security requirement
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .addServersItem(new Server().url("/api"))
-                .addSecurityItem(new SecurityRequirement()
-                        .addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
-                .info(apiInfo());
+                .info(new Info()
+                        .title("StackFlow API")
+                        .description("StackFlow API 명세서")
+                        .version("v1.0")
+                        .contact(new Contact()
+                                .name("StackFlow")
+                                .email("rokmc17047200@gmail.com")))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement);
     }
-
-    // 매장 관리 그룹
-    @Bean
-    public GroupedOpenApi signupApi() {
-        return GroupedOpenApi.builder()
-                .group("매장 관리")
-                .pathsToMatch("/users/**") // 매장 관리 경로만 문서화
-                .build();
-    }
-
-//    @Bean
-//    public GroupedOpenApi adminApi() {
-//        return GroupedOpenApi.builder()
-//                .group("관리자")
-//                .pathsToMatch("/admin/**")
-//                .build();
-    }
+}
